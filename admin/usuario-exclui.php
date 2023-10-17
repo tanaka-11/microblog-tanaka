4319cbce01650872090cb53a18d3d81075f1c0c9
Marina Tanaka <huttenloucher@gmail.com>
2023-10-17 09:15:45 -0300

    ajuste para funcionamento do metodo de verificação

--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -3,14 +3,14 @@
 use Microblog\{Usuario, ControleDeAcesso};
 require_once '../vendor/autoload.php';
 
+// Criação do objeto e executando metodo para verificação de login
+$sessao = new ControleDeAcesso;
+$sessao->verificaAcesso();
+
 // Script de exclusão
 $usuario = new Usuario;
 $usuario->setId($_GET['id']);
 $usuario->deletar();
-
-// Criação do objeto e executando metodo para verificação de login
-$sessao = new ControleDeAcesso;
-$sessao->verificaAcesso();
 
 // Redirecionamento
 header('location:usuarios.php');
