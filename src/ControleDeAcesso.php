f60d3e3ed1dc5030e1f3c075f852fd6a7b090319
Marina Tanaka <huttenloucher@gmail.com>
2023-10-17 08:57:19 -0300

    criado metodos para iniciar a sessão e verificação de login

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -4,7 +4,7 @@ namespace Microblog;
 
 // Classe
 final class ControleDeAcesso {
-    // Metodo para iniciar a sessão($_SESSION) via construtor.
+    // Metodo para iniciar a sessão com o array superglobal($_SESSION) via construtor.
     public function __construct(){
         // Se não existir uma sessão em funcionamento ele vai se iniciar com o comando (session_start)
         if(!isset($_SESSION)){
@@ -14,10 +14,11 @@ final class ControleDeAcesso {
 
     // Metodo para verificação de acesso
     public function verificaAcesso():void {
+        // Se  não existir uma variavel de sessão chamada "id", ou seja, não logado então termine a sessão e retorne para o login
         if (!isset($_SESSION['id'])) {
             session_destroy();
             header('location:../login.php');
-            die();
+            die(); // ou o comando exit();
         }
     }
 }
