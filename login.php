4d7e60baa874db63ac3a60162e36558a7af06f6a
Marina Tanaka <huttenloucher@gmail.com>
2023-10-17 09:55:27 -0300

    Executando verificação de login

--- login.php
+++ login.php
@@ -1,5 +1,13 @@
 <?php 
+// Namespace e require
+use Microblog\{Usuario, ControleDeAcesso};
 require_once "inc/cabecalho.php";
+
+// Programação das mensagens de feedback sobre login dos usuarios
+if(isset($_GET['campos_obrigatorios'])) {
+	$feedback = "Você deve fazer o login primeiro!";
+}
+
 ?>
 
 
@@ -9,13 +17,15 @@ require_once "inc/cabecalho.php";
 
         <form action="" method="post" id="form-login" name="form-login" class="mx-auto w-50">
 
-                
-				<p class="my-2 alert alert-warning text-center"></p>
+                <?php if(isset($feedback)) {?>
+				<p class="my-2 alert alert-warning text-center"><?=$feedback?></p>
+				<?php } ?>
 
 				<div class="mb-3">
 					<label for="email" class="form-label">E-mail:</label>
 					<input class="form-control" type="email" id="email" name="email">
 				</div>
+
 				<div class="mb-3">
 					<label for="senha" class="form-label">Senha:</label>
 					<input class="form-control" type="password" id="senha" name="senha">
@@ -24,6 +34,19 @@ require_once "inc/cabecalho.php";
 				<button class="btn btn-primary btn-lg" name="entrar" type="submit">Entrar</button>
 
 			</form>
+
+			<?php
+				if(isset($_POST['entrar'])) {
+					// Verificando se os campos 'email' e 'senha' estão vazio.
+					if (empty($_POST['email']) || empty($_POST['senha'])) {
+						// Passando o parametro 'campos_obrigatorios' na url
+						header('location:login.php?campos_obrigatorios');
+					} else {
+						echo 'Dados okay';
+					}
+				}
+			?>
+
     </div>
     
     
