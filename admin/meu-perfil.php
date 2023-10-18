0cc5df261710d9b0683a83e4c4decb6d0ed2ff41
Marina Tanaka <huttenloucher@gmail.com>
2023-10-18 10:00:42 -0300

    Adicionado programação de atualização do perfil

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,39 @@
 <?php 
+// Namespace e require
+use Microblog\{Usuario, Utilitarios};
 require_once "../inc/cabecalho-admin.php";
+
+// Criação do objeto e atribuição do objeto ID do usuario logado
+$usuario = new Usuario;
+$usuario->setId($_SESSION['id']);
+
+// Instancia
+$dadosDoUsuario = $usuario->listarUm();
+
+// Script de atualização
+if(isset($_POST['atualizar'])) {
+	$usuario->setNome($_POST['nome']);
+	$usuario->setEmail($_POST['email']);
+	$usuario->setTipo($_SESSION['tipo']); // Utilizamos o Session pois passamos o tipo no metodo atualizar e estamos trazendo o tipo da pessoa que esta logada(da propria sessão)
+
+	// Verificação da senha
+	if(empty($_POST['senha'])) {
+		$usuario->setSenha($dadosDoUsuario['senha']);
+	} else {
+		$usuario->setSenha(
+			$usuario->verificaSenha($_POST['senha'], $dadosDoUsuario['senha'])
+		);
+	}
+
+	// Chamando o metodo de atualização
+	$usuario->atualizar();
+	
+	// Atualizando a variavel de sessão com o novo nome
+ 	$_SESSION['nome'] = $usuario->getNome();
+
+	// Redirecionamento apos atualização
+	header('location:index.php?perfil_atualizado');
+}
 ?>
 
 
@@ -14,12 +48,12 @@ require_once "../inc/cabecalho-admin.php";
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?=$dadosDoUsuario['nome']?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?=$dadosDoUsuario['email']?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
