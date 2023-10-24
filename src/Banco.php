c64fd7dc1b11d6e46ae5712786edf0a98a681907
Marina Tanaka <huttenloucher@gmail.com>
2023-10-24 09:26:08 -0300

    refatoração da classe do Banco

--- src/Banco.php
+++ src/Banco.php
@@ -7,20 +7,26 @@ abstract class Banco {
     private static string $usuario = "root";
     private static string $senha = "";
     private static string $banco = "microblog_tanaka";
-    private static PDO $conexao; 
+    
+    // Operador (?) "nullable typeint" (PHP +7.1) 
+    // ele indica que a propriedade da classe(conexão) pode ser nulo ou do tipo PDO
+    private static ?PDO $conexao = null; 
 
     public static function conecta():PDO {
-        try {
-            self::$conexao = new PDO(
-                "mysql:host=".self::$servidor."; 
-                dbname=".self::$banco.";
-                charset=utf8",
-                self::$usuario, 
-                self::$senha
-            );
-            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $erro) {
-            die("Deu ruim: ".$erro->getMessage());
+        // Condicional para saber se existe uma conexão
+        if(self::$conexao === null){
+            try {
+                self::$conexao = new PDO(
+                    "mysql:host=".self::$servidor."; 
+                    dbname=".self::$banco.";
+                    charset=utf8",
+                    self::$usuario, 
+                    self::$senha
+                );
+                self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (Exception $erro) {
+                die("Deu ruim: ".$erro->getMessage());
+            }
         }
         return self::$conexao;
     }
