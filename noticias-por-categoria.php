162202190aaa36583f79ec0ea063a2e15ada1c86
Marina Tanaka <huttenloucher@gmail.com>
2023-10-27 09:45:01 -0300

    refatoração para exibir as noticias por categoria

--- noticias-por-categoria.php
+++ noticias-por-categoria.php
@@ -1,34 +1,31 @@
-<?php 
+<?php
+
 require_once "inc/cabecalho.php";
 $noticia->categoria->setId($_GET['id']);
-// $dados = $noticia->listarCategorias();
+$dados = $noticia->listarCategorias();
+
 ?>
 
 
 <div class="row my-1 mx-md-n1">
 
     <article class="col-12">
-        <h2 class=" ">Notícias sobre <span class="badge bg-primary">categoria</span> </h2>
-        
+    <?php if(count($dados) > 0) {?>
+        <h2 class="text-center">Notícias sobre <span class="badge bg-primary"><?=$dados[0]['categoria']?></span> </h2>
+    <?php } else { ?>
+        <h2 class="alert alert-warning text-center">Não há notícias desta categoria</h2>           
+    <?php } ?>    
+
         <div class="row my-1">
             <div class="col-12 px-md-1">
                 <div class="list-group">
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
+                <?php foreach($dados as $noticiaPorCategoria){?>
+                    <a href="noticia.php?id=<?=$noticiaPorCategoria['id']?>" class="list-group-item list-group-item-action">
+                        <h3 class="fs-6"><?=$noticiaPorCategoria['titulo']?></h3>
+                        <p><time><?=$noticiaPorCategoria['data']?></time> - <?=$noticiaPorCategoria['autor']?></p>
+                        <p><?=$noticiaPorCategoria['resumo']?></p>
                     </a>
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
-                    
+                <?php } ?>
                 </div>
             </div>
         </div>
